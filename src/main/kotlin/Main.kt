6ac400369dd6c4a560aeaaacff5f6d5a36f3bfdf
Natete-Verite <nateteverite@gmail.com>
2022-02-14 21:39:46 +0300

    first kotlin project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+
+   var student = arrayOf ("Super", 24, +250789415454, 70.5)
+    println(student [2])
+
+    var nationality : Boolean = false
+    println(nationality)
+
+
+
+
+}
